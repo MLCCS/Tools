4be8b043bfd6a842e61c28de5cb23e34af2b0376
MLCCS <554691952@qq.com>
2019-04-21 17:03:19 +0800

    通过

--- arpwatch.py
+++ arpwatch.py
@@ -2,24 +2,29 @@
 from scapy.all import sniff,ARP
 from signal import signal,SIGINT
 import sys
+import argparse
 
 
 ip_mac = {}
 def watch_arp(pkt):
-	print(pkt)
+	#	print(pkt)
 	# 如果是ARP响应包，打印
 	if pkt[ARP].op == 2:
-		print(str(pkt[ARP].hwsrc) + "\t" + str(pkt[ARP].psrc))
+		print(pkt[ARP].hwsrc + "\t" + pkt[ARP].psrc)
 	# 如果是新设备，则加入字典
 	if ip_mac.get(pkt[ARP].psrc) == None:
 		print("Found new device:%s\t%s" %(pkt[ARP].hwsrc,
 			pkt[ARP].psrc))
-		ip_mac[pkt[ARP].psrc] == pkt[ARP].hwsrc
+		ip_mac[pkt[ARP].psrc] = pkt[ARP].hwsrc
 	# 某ip对应的MAC地址更换了，有点不正常，提示一下
-	elif ip_mac.get(pkt[ARP].psrc) and ip_mac[pkt[ARP].pwsrc] != pkt[ARP].hwsrc:
+	elif ip_mac.get(pkt[ARP].psrc) and ip_mac[pkt[ARP].psrc] != pkt[ARP].hwsrc:
 		print("[x] %s has got a new MAC:%s,it's old MAC is:%s" %(pkt[ARP].psrc,
 			pkt[ARP].hwsrc,ip_mac.get(pkt[ARP].psrc)))
 		ip_mac[pkt[ARP].psrc] = pkt[ARP].hwsrc
 
 if __name__ == '__main__':
-	sniff(prn=watch_arp,filter="arp",iface="eth0",store=0)
+	parser = argparse.ArgumentParser('MAC_Flood')
+	parser.add_argument(dest='interface',metavar='interface')
+	args = parser.parse_args()
+	iface = args.interface
+	sniff(prn=watch_arp,filter="arp",iface=iface,store=0)
--- macflood.py
+++ macflood.py
@@ -4,15 +4,17 @@ RandMAC()随机产生mac
 RandIP随机产生ip 
 都可以接收模板，产生指定要求的mac或ip
 """
+import argparse
 import sys
 import time
 from scapy.all import *
 
 
 def mac_flood():
-	iface = "eth0"
-	if len(sys.argv) >= 2:
-		iface = sys.argv[1]
+	parser = argparse.ArgumentParser('MAC_Flood')
+	parser.add_argument(dest='interface',metavar='interface')
+	args = parser.parse_args()
+	iface = args.interface
 	while 1:
 		pkt = Ether(src=RandMAC(),dst=RandMAC()) / IP(
 			src=RandIP(),dst=RandIP()) / ICMP()
